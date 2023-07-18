1182ab7e538756b723ac50aaf4e4435475290377
EGAMER26 <evandrogomes542@gmail.com>
2023-07-18 13:46:12 -0300

    feita a instalação do eslint

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,19 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-// http Methos: 
+// http Methos:
 // POST(exemplo: cadastrar usuario)
 // GET(exemplo: listar usuario)
 // PUT(exemplo: atualizar alguma coisa)
 // DELETE(exemplo: deletar alguma coisa)
 // PATCH(exemplo: para atualizar uma informação especifica dentro de um recurso)
-app.get("/hello", () => {
+app.get('/hello', () => {
   return 'hello world'
 })
-
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('Server listening on port 3333')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server listening on port 3333')
+  })
