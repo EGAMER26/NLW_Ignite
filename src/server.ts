7eea95139a8dc8e82b5bd468ead2694e54bbccdd
EGAMER26 <evandrogomes542@gmail.com>
2023-07-18 14:39:02 -0300

    adicionado prisma e o banco SQlite

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
-
+const prisma = new PrismaClient()
 // http Methos:
 // POST(exemplo: cadastrar usuario)
 // GET(exemplo: listar usuario)
 // PUT(exemplo: atualizar alguma coisa)
 // DELETE(exemplo: deletar alguma coisa)
 // PATCH(exemplo: para atualizar uma informação especifica dentro de um recurso)
-app.get('/hello', () => {
-  return 'hello world'
+app.get('/hello', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
 })
 app
   .listen({
